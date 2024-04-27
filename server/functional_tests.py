cb87b468cfa0295f76383325276856ed3b03e412
viktor d <10xl.pro@mail.ru>
2024-04-27 13:50:50 +0300

    Добавлен функция в ФТ для тестирования строк в таблице

--- server/functional_tests.py
+++ server/functional_tests.py
@@ -17,6 +17,12 @@ class NewVisitorTest(unittest.TestCase):
         '''Удаление'''
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        '''Подтверждение наличия строки в таблице списка'''
+        table = self.browser.find_element(By.ID, 'id_list_table')
+        rows = table.find_elements(By.TAG_NAME, 'tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrive_it_later(self):
         '''Тест: можно начать список и получить его позже'''
         # Эдит слышала про крутое онлайн-приложение со списком
@@ -46,9 +52,7 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        table = self.browser.find_element(By.ID, 'id_list_table')
-        rows = table.find_elements(By.TAG_NAME, 'tr')
-        self.assertIn('1: Купить павлиньи перья', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Купить павлиньи перья')
 
         # Текстовое поле по-прежнему приглашает ее добавить еще один элемент.
         # Она вводит "Сделать мушку из павлиньих перьев"
@@ -58,10 +62,8 @@ class NewVisitorTest(unittest.TestCase):
         time.sleep(1)
         
         # Страница снова обновляется, и теперь показывает оба элемента ее списка
-        table = self.browser.find_element(By.ID, 'id_list_table')
-        rows = table.find_elements(By.TAG_NAME, 'tr')
-        self.assertIn('1: Купить павлиньи перья', [row.text for row in rows])
-        self.assertIn('2: Сделать мушку из павлиньих перьев', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Купить павлиньи перья')
+        self.check_for_row_in_list_table('2: Сделать мушку из павлиньих перьев')
 
         # Эдит интересно, запомнит ли сайт ее список. Далее она видит, что
         # сайт сгенерировал для нее уникальный URL-адрес - об этом
