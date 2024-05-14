bfe59a908d511f8ee7ca06b7e98f59b301b81985
viktor d <10xl.pro@mail.ru>
2024-05-14 19:46:41 +0300

    Немного обновлений

--- server/server/settings.py
+++ server/server/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5e5m$!kxp35ln9kt($l71ei9m=91=g-1qy($#1_a^kh+g3&bdr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 TEMPLATE_DEBUG = DEBUG
 
@@ -121,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '../../static/'))
+STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '../static/'))
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
