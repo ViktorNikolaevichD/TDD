c777cc4fa9c14d569382bf73faa4c3c7857ec22e
viktor d <10xl.pro@mail.ru>
2024-05-18 18:30:13 +0300

    Перемещены ФТ в свои собственные отдельные файлы

---
+++ server/functional_tests/base.py
@@ -0,0 +1,36 @@
+import time
+
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import WebDriverException
+
+MAX_WAIT = 10
+
+class FunctionalTest(StaticLiveServerTestCase):
+    '''Функциональный тест'''
+
+    def setUp(self):
+        '''Установка'''
+        self.browser = webdriver.Firefox()
+        staging_server = "rememberlist.store" #  os.environ.get('STAGING_SERVER')
+        if staging_server:
+            self.live_server_url = 'https://' + staging_server
+
+    def tearDown(self):
+        '''Удаление'''
+        self.browser.quit()
+
+    def wait_for_row_in_list_table(self, row_text):
+        '''Подтверждение наличия строки в таблице списка'''
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element(By.ID, 'id_list_table')
+                rows = table.find_elements(By.TAG_NAME, 'tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
--- server/functional_tests/test_simple_list_creation.py
+++ server/functional_tests/test_simple_list_creation.py
@@ -1,43 +1,11 @@
-import time
-import os
-import unittest
-
-from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from .base import FunctionalTest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import WebDriverException
 
-MAX_WAIT = 10
 
-class NewVisitorTest(StaticLiveServerTestCase):
+class NewVisitorTest(FunctionalTest):
     '''Тест нового посетителя'''
-
-    def setUp(self):
-        '''Установка'''
-        self.browser = webdriver.Firefox()
-        staging_server = os.environ.get('STAGING_SERVER')
-        if staging_server:
-            self.live_server_url = 'https://' + staging_server
-
-    def tearDown(self):
-        '''Удаление'''
-        self.browser.quit()
-
-    def wait_for_row_in_list_table(self, row_text):
-        '''Подтверждение наличия строки в таблице списка'''
-        start_time = time.time()
-        while True:
-            try:
-                table = self.browser.find_element(By.ID, 'id_list_table')
-                rows = table.find_elements(By.TAG_NAME, 'tr')
-                self.assertIn(row_text, [row.text for row in rows])
-                return
-            except (AssertionError, WebDriverException) as e:
-                if time.time() - start_time > MAX_WAIT:
-                    raise e
-                time.sleep(0.5)
-
     def test_can_start_a_list_for_one_user(self):
         '''Тест: можно начать список и получить его позже'''
         # Эдит слышала про крутое онлайн-приложение со списком
@@ -125,29 +93,3 @@ class NewVisitorTest(StaticLiveServerTestCase):
         page_text = self.browser.find_element(By.TAG_NAME, 'body').text
         self.assertNotIn('Купить павлиньи перья', page_text)
         self.assertIn('Купить молоко', page_text)
-        
-    def test_layout_and_styling(self):
-        '''Тест: макет и стилевое оформление'''
-        # Эдит открывает домашнюю страницу
-        self.browser.get(self.live_server_url)
-        self.browser.set_window_size(1024, 768)
-
-        # Она замечает, что поле ввода аккуратно центрировано
-        inputbox = self.browser.find_element(By.ID, 'id_new_item')
-        self.assertAlmostEqual(
-            inputbox.location['x'] + inputbox.size['width'] / 2,
-            512,
-            delta=10
-        )
-
-        # Она начинает новый список и видит, что поле ввода там тоже
-        # аккуратно центрировано
-        inputbox.send_keys('testing')
-        inputbox.send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table('1: testing')
-        inputbox = self.browser.find_element(By.ID, 'id_new_item')
-        self.assertAlmostEqual(
-            inputbox.location['x'] + inputbox.size['width'] / 2,
-            512,
-            delta=10
-        )
