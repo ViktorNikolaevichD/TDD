c999e6c5bfa512225b15085f23c6e240d2d854b6
viktor d <10xl.pro@mail.ru>
2024-06-05 13:24:13 +0300

    добавлена реакция на клик по форме, который убирает ошибку, добавлено простр-во имен

--- server/lists/static/list.js
+++ server/lists/static/list.js
@@ -1,5 +1,6 @@
-var initialize = function () {
-    $('input[name="text"]').on('keypress', function () {
+window.Superlists = {};
+window.Superlists.initialize = function () {
+    $('input[name="text"]').on('click keypress', function () {
         $('.has-error').hide();
     });
 };
