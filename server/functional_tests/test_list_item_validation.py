cf04df2040105c15cc9bf0c6a1799f5f2a68911d
viktor d <10xl.pro@mail.ru>
2024-06-05 10:55:11 +0300

    Первая проверка фт для ошибки валидации

--- server/functional_tests/test_list_item_validation.py
+++ server/functional_tests/test_list_item_validation.py
@@ -6,6 +6,11 @@ from selenium.webdriver.common.by import By
 
 class ItemValidationTest(FunctionalTest):
     '''Тест валидации элемента списка'''
+
+    def get_error_element(self):
+        '''Получить элемент с ошибкой'''
+        return self.browser.find_element(By.CSS_SELECTOR, '.has-error')
+    
     def test_cannot_add_empty_list_items(self):
         '''Тест: нельзя добавлять пустые элементы списка'''
         # Эдит открывает домашнюю страницу и случайно пытается отправить
@@ -72,6 +77,28 @@ class ItemValidationTest(FunctionalTest):
         
         # Она видит полезное сообщение об ошибке
         self.wait_for(lambda: self.assertEqual(
-            self.browser.find_element(By.CSS_SELECTOR, '.has-error').text,
+            self.get_error_element().text,
             "You've already got this in your list"
+        ))
+    
+    def test_error_messages_are_cleared_on_input(self):
+        '''Тест: сообщения об ошибках очищаются при вводе'''
+        # Эдит начинает список и вызывает ошибку валидации:
+        self.browser.get(self.live_server_url)
+        self.get_item_input_box().send_keys('Banter to thick')
+        self.get_item_input_box().send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Banter to thick')
+        self.get_item_input_box().send_keys('Banter to thick')
+        self.get_item_input_box().send_keys(Keys.ENTER)
+
+        self.wait_for(lambda: self.assertTrue(
+            self.get_error_element().is_displayed()
+        ))
+
+        # Она начинает набирать в поле ввода, чтобы очистить ошибку
+        self.get_item_input_box().send_keys('a')
+
+        # Она довольна от того, что сообщение об ошибке исчезает
+        self.wait_for(lambda: self.assertFalse(
+            self.get_error_element().is_displayed()
         ))
